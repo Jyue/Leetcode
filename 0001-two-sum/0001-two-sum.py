ef24aa24c3d3b33472879ade0502e02b10a5828e
Cindy Lin <Jyue@users.noreply.github.com>
2022-12-26 22:04:45 +0800

    Time: 74 ms (80.96%), Space: 15.2 MB (52.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,34 +1,10 @@
-def binarySearch(nums: List[int], target: int):
-    left = 0
-    right = len(nums) -1
-    
-    while (left <= right):
-        mid = (left+right) // 2
-        if target < nums[mid]:
-            right = mid -1
-        elif target > nums[mid]:
-            left = mid + 1
-        else:
-            return nums[mid]
-    return float("-inf")
-    
-    
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sorted_nums = sorted(nums)
-        
-        for i in range(len(sorted_nums)):
-            i_val = sorted_nums[i]
-            j_val = binarySearch(sorted_nums[i+1:],target-sorted_nums[i])
-            if j_val != float("-inf"):
-                break
-      
-        print(i_val,j_val)
-        
-        indices = []
+        hashmap = {}
         for i in range(len(nums)):
-            if nums[i] in [i_val,j_val]:
-                indices.append(i)
-                if len(indices) == 2:
-                    return indices
-            
+            hashmap[nums[i]] = i
+        print(hashmap)
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
