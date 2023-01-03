21010fabb4ff31b77fa5cdb848cd39793aa65ca7
Cindy Lin <Jyue@users.noreply.github.com>
2023-01-04 00:00:53 +0800

    Time: 320 ms (43.17%), Space: 17.1 MB (10.09%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,3 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        appear_list = []
-        for num in nums:
-            if num not in appear_list:
-                appear_list.append(num)
-            else:
-                appear_list.remove(num)
-        return appear_list[0]
+        return 2 * sum(set(nums)) - sum(nums)
