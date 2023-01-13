d52d147d4cbab559d892b6cdaad04dc37f8d92b5
Cindy Lin <Jyue@users.noreply.github.com>
2023-01-13 13:34:29 +0800

    Time: 86 ms (70.58%), Space: 18.8 MB (22.42%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,49 +1,26 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        def dfs(node):  # Return [How many target finded, the lowestCommonAncestor(TreeNode)]
-            # print(node)
-            if not node:
-                return (0,None)
-            
-            if not node.left and not node.right:
-                # print(node.val,"is a leaf",node.val == p or node.val == q)
-                return (1,None) if (node.val == p.val or node.val == q.val) else (0,None)
-            
-            
-            
-            left, leftAncestorFind = dfs(node.left)
-            right, rightAncestorFind = dfs(node.right)
-            
-            if leftAncestorFind or rightAncestorFind:
-                # print(node.val,"has found target")
-                if leftAncestorFind:
-                    return (2,leftAncestorFind)
-                else:
-                    return (2,rightAncestorFind)
-                
-            
-            if node.val == p.val or node.val == q.val:
-                count = 1 + left + right
-            else:
-                count = left + right
-            
-            # print(node.val, count)
-            return (2,node) if count == 2 else (count,None)
-   
-                
-        # print(dfs(root))
-        # return 0
-        _,lowestCommonAncestor = dfs(root)
-        # print(lowestCommonAncestor)
-        
-        return lowestCommonAncestor
-        
-        
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        # Value of current node or parent node.
+        parent_val = root.val
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # If both p and q are greater than parent
+        if p_val > parent_val and q_val > parent_val:    
+            return self.lowestCommonAncestor(root.right, p, q)
+        # If both p and q are lesser than parent
+        elif p_val < parent_val and q_val < parent_val:    
+            return self.lowestCommonAncestor(root.left, p, q)
+        # We have found the split point, i.e. the LCA node.
+        else:
+            return root
