838c2eab61f96599ffd789f736c758b4f7c85975
Cindy Lin <Jyue@users.noreply.github.com>
2023-01-25 06:09:43 +0800

    Time: 4102 ms (50.72%), Space: 42.1 MB (28.71%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,46 +1,61 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        arr = [[v, i] for i, v in enumerate(nums)]  # record value and index
-        result = [0] * n
 
-        def merge_sort(arr, left, right):
-            # merge sort [left, right) from small to large, in place
-            if right - left <= 1:
-                return
-            mid = (left + right) // 2
-            merge_sort(arr, left, mid)
-            merge_sort(arr, mid, right)
-            merge(arr, left, right, mid)
-
-        def merge(arr, left, right, mid):
-            # merge [left, mid) and [mid, right)
-            i = left  # current index for the left array
-            j = mid  # current index for the right array
-            # use temp to temporarily store sorted array
-            temp = []
-            while i < mid and j < right:
-                if arr[i][0] <= arr[j][0]:
-                    # j - mid numbers jump to the left side of arr[i]
-                    result[arr[i][1]] += j - mid
-                    temp.append(arr[i])
+        def merge(left, right):
+            nonlocal result
+            mergeResult = []
+            i = 0
+            j = 0
+            left.append([-float(inf),0])
+            right.append([float(inf),0])
+            while i < len(left) and j < len(right):
+                if left[i][0] == float(-inf):
                     i += 1
+                    break
+                if left[i][0] <= right[j][0]:
+                    mergeResult.append(left[i])
+                    # left[i][1] += j
+                    result[left[i][1]] += j
+                    
+                    i += 1
+                    
                 else:
-                    temp.append(arr[j])
+                    mergeResult.append(right[j])
+                    # for count in left:
+                    #     count[1] += 1
                     j += 1
-            # when one of the subarrays is empty
-            while i < mid:
-                # j - mid numbers jump to the left side of arr[i]
-                result[arr[i][1]] += j - mid
-                temp.append(arr[i])
-                i += 1
-            while j < right:
-                temp.append(arr[j])
-                j += 1
-            # restore from temp
-            for i in range(left, right):
-                arr[i] = temp[i - left]
+            
+            if i < len(left) and left[i][0] != float(-inf):
+                mergeResult += left[i: len(left) - 1]
+            if j < len(right) and right[j][0] != float(inf):
+                mergeResult += right[j: len(right) - 1]
+               
+            # print(result)
+            # print(left, "----", right,"----------", mergeResult)
+            # print()
+            return mergeResult
+            
+        def mergesort(array):
+            if len(array) < 2:
+                return array
+            mid = len(array) //2
+            return merge(mergesort(array[:mid]), mergesort(array[mid:]))
+        
+        
 
-        merge_sort(arr, 0, n)
-
+        # print(len(nums))        
+        result = [0] *len(nums)
+        count = []
+        
+        for idx, num in enumerate(nums):
+            count.append([num, idx])
+        
+        mergesort(count)
+        # print(count)
+        
+    
+        # return [x[1] for x in count]
         return result
+        
+        
+        
