c2b37f1285895c9c8447f43c3300cf58461f40f7
Cindy Lin <Jyue@users.noreply.github.com>
2023-02-02 13:23:49 +0800

    Time: 37 ms (77.62%), Space: 13.8 MB (77.66%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -3,19 +3,22 @@ class Solution:
         n = len(words)
         
         for word1, word2 in zip(words[:n-1],words[1:]):
+   
             word1len = len(word1)
             word2len = len(word2)
-            isSamePage = True
+        
+            if word1[:word2len] == word2 and word1len > word2len:
+                return False
+            
+   
             i = 0
             while i < min(word1len, word2len):
                 # print(i, word1[i], word2[i])
                 if order.index(word1[i]) > order.index(word2[i]):
                     return False
                 elif order.index(word1[i]) < order.index(word2[i]):
-                    isSamePage = False
                     break
                 i += 1
-            if isSamePage and word1len > word2len:
-                return False
+            
         
         return True
