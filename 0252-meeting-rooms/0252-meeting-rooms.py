2b8ce1fa41812486a7558dec0922a9ab0de67cd5
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-04 22:30:41 +0800

    Time: 103 ms (9.94%), Space: 19.8 MB (44.68%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,16 +1,13 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        self.meeting_array = [0]*1000001
-        self.max_end_time = 0
-        for interval in intervals:
-            start, end = interval[0],interval[1]
-            self.meeting_array[start] += 1
-            self.meeting_array[end] -= 1
-            if end > self.max_end_time:
-                self.max_end_time = end
-        meeting_count = 0
-        for i in range(self.max_end_time):
-            meeting_count += self.meeting_array[i]
-            if meeting_count > 1:
+        heapq.heapify(intervals)
+        max_till_now = 0
+        
+        while intervals:
+            start, end = heapq.heappop(intervals)
+            if start < max_till_now:
                 return False
+            else:
+                max_till_now = max(max_till_now, end)
+             
         return True
