908f3f073674c351fe61656e9197871fb8b8e446
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-18 08:41:09 +0800

    Time: 142 ms (49.22%), Space: 17.3 MB (48.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,54 +1,37 @@
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
+class Solution(object):
+    def setZeroes(self, matrix):
         """
-        Do not return anything, modify matrix in-place instead.
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
         """
-        m = len(matrix)
-        n = len(matrix[0])
-        # 先確認第一列和第一行清0狀況，並將之記錄在左上角的欄位。之後用第一列和第一行當作 flag
-        
-        # Step1: flag常數用來確認第一列和第一行最後是否為 0
-        # 0: 不改，1: 第一列和第一行清0，2: 第一row清0，3: 第一col清0
-        flag = 0
-        
-        
-        if matrix[0][0] == 0:
-            flag = 1
-        else:
-            for i in range(n):
-                if matrix[0][i] == 0:
-                    flag = 2
-                    break
-            for j in range(m):
-                if matrix[j][0] == 0:
-                    if flag == 2:
-                        flag = 1
-                    else:
-                        flag = 3
-                    break   
-        # Step2: 第一列和第一行當flag，確認每一行每一列是否清0
-        # 0: 清0
-        for i in range(1,m):
-            for j in range(1,n):
-                if matrix[i][j] == 0:
-                    matrix[i][0] = 0
-                    matrix[0][j] = 0
-                    
-        # Step3: 根據左上角，將每一行每一列清0
-        for i in range(1,m):
+        is_col = False
+        R = len(matrix)
+        C = len(matrix[0])
+        for i in range(R):
+            # Since first cell for both first row and first column is the same i.e. matrix[0][0]
+            # We can use an additional variable for either the first row/column.
+            # For this solution we are using an additional variable for the first column
+            # and using matrix[0][0] for the first row.
             if matrix[i][0] == 0:
-                for k in range(1,n):
-                    matrix[i][k] = 0
-        for j in range(1,n):
-            if matrix[0][j] == 0:
-                for k in range(1,m):
-                    matrix[k][j] = 0
-                
-        # Step4: 根據flag常數，將第一列和第一行清0
-        if flag == 1 or flag == 2:
-            for i in range(n):
-                matrix[0][i] = 0
-        if flag == 1 or flag == 3:
-            for i in range(m):
+                is_col = True
+            for j in range(1, C):
+                # If an element is zero, we set the first element of the corresponding row and column to 0
+                if matrix[i][j]  == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+
+        # Iterate over the array once again and using the first row and first column, update the elements.
+        for i in range(1, R):
+            for j in range(1, C):
+                if not matrix[i][0] or not matrix[0][j]:
+                    matrix[i][j] = 0
+
+        # See if the first row needs to be set to zero as well
+        if matrix[0][0] == 0:
+            for j in range(C):
+                matrix[0][j] = 0
+
+        # See if the first column needs to be set to zero as well        
+        if is_col:
+            for i in range(R):
                 matrix[i][0] = 0
-            
