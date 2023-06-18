1a31cd4b4fbcf1bf053f50f28f25ba6cd585d728
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-18 09:00:24 +0800

    Time: 152 ms (14.21%), Space: 17.2 MB (48.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,32 +1,11 @@
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-
-        m = len(matrix)
-        n = len(matrix[0])
-		
-        first_row_has_zero = False
-        first_col_has_zero = False
-        
-        # iterate through matrix to mark the zero row and cols
-        for row in range(m):
-            for col in range(n):
-                if matrix[row][col] == 0:
-                    if row == 0:
-                        first_row_has_zero = True
-                    if col == 0:
-                        first_col_has_zero = True
-                    matrix[row][0] = matrix[0][col] = 0
-    
-        # iterate through matrix to update the cell to be zero if it's in a zero row or col
-        for row in range(1, m):
-            for col in range(1, n):
-                matrix[row][col] = 0 if matrix[0][col] == 0 or matrix[row][0] == 0 else matrix[row][col]
-        
-        # update the first row and col if they're zero
-        if first_row_has_zero:
-            for col in range(n):
-                matrix[0][col] = 0
-        
-        if first_col_has_zero:
-            for row in range(m):
-                matrix[row][0] = 0
+    def setZeroes(self, m: List[List[int]]) -> None:
+    	M, N = len(m), len(m[0])
+    	for i,j in itertools.product(range(M),range(N)):
+    		if m[i][j]: continue
+    		for k in range(N):
+    			if m[i][k] != 0: m[i][k] = ' '
+    		for k in range(M):
+    			if m[k][j] != 0: m[k][j] = ' '
+    	for i,j in itertools.product(range(M),range(N)):
+    		if m[i][j] == ' ': m[i][j] = 0
