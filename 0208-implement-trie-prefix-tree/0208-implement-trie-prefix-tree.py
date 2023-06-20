21ec9a361e5256060655b37c72ef8315dccd1b32
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-20 13:31:26 +0800

    Time: 124 ms (99.05%), Space: 30.7 MB (82.63%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,63 +1,27 @@
-class TrieNode:
-    """A node in the trie structure"""
+class Trie(object):
 
-    def __init__(self, char):
-        # the character stored in this node
-        self.char = char
-
-        # whether this can be the end of a word
-        self.is_end = False
-
-        # a dictionary of child nodes
-        # keys are characters, values are nodes
-        self.children = {}
-
-class Trie:
-
-    def __init__(self):
-        self.root = TrieNode("")
+	def __init__(self):
+		self.trie = {}
 
 
-    def insert(self, word: str) -> None:
-        node = self.root
-        
-        # Loop through each character in the word
-        # Check if there is no child containing the character, create a new child for the current node
-        for char in word:
-            if char in node.children:
-                node = node.children[char]
-            # If a character is not found,
-            # create a new node in the trie
-            else:
-                new_node = TrieNode(char)
-                node.children[char] = new_node
-                node = new_node
-                
-        # Mark the end of a word
-        node.is_end = True
-
-       
-
-    def search(self, word: str) -> bool:
-        node = self.root
-        for char in word:
-            if char in node.children:
-                node = node.children[char]
-            else:
-                return False
-        return node.is_end
-
-    def startsWith(self, prefix: str) -> bool:
-        node = self.root
-        for char in prefix:
-            if char not in node.children:
-                return False
-            node = node.children[char]
-        return True
+	def insert(self, word):
+		t = self.trie
+		for c in word:
+			if c not in t: t[c] = {}
+			t = t[c]
+		t["-"] = True
 
 
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
+	def search(self, word):
+		t = self.trie
+		for c in word:
+			if c not in t: return False
+			t = t[c]
+		return "-" in t
+
+	def startsWith(self, prefix):
+		t = self.trie
+		for c in prefix:
+			if c not in t: return False
+			t = t[c]
+		return True
