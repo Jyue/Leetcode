bf003ae7b4efef813e7f41511fa6aa868161133b
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-20 08:23:01 +0800

    Time: 52 ms (76.11%), Space: 16.8 MB (29.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,20 +1,31 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Initilize two pointers
-        begin = 0
-        end = len(nums) - 1 
-        while begin <= end:
-            mid = (begin + end)//2
+        n = len(nums)
+        l = 0
+        r = n - 1
+        
+        if n == 1:
+            return 0 if nums[0] == target else -1
+        if n == 2:
+            if target not in nums:
+                return -1
+            else:
+                return 0 if nums[0] == target else 1
+        
+        while l <= r:
+            mid = (l + r)//2
+            
             if nums[mid] == target:
                 return mid
-            if nums[mid] >= nums[end]: # Left side of mid is sorted
-                if  nums[begin] <= target and target < nums[mid]: # Target in the left side
-                    end = mid - 1
-                else: # in right side
-                    begin = mid + 1
-            else: # Right side is sorted
-                if  nums[mid] < target and target <= nums[end]: # Target in the right side
-                    begin = mid + 1
-                else: # in left side
-                    end = mid - 1
+            
+            elif target >  nums[mid]:
+                if nums[l] > nums[r] and target > nums[r] and nums[l] > nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[l] > nums[r] and target < nums[l] and nums[r] < nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
         return -1
