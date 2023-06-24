e9c0dd05283ef85250856b2838dfe7d0eaa47d37
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-25 05:48:14 +0800

    Time: 116 ms (12.23%), Space: 26.4 MB (15.54%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,34 +1,22 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        # Find a Middle Node
-        # in 1->2->3->4->5->6 find 4 
-        slow = head
-        fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        #print(slow.val)
-        
-        
-        
-        # Reverse Linked list
-        prev, curr = None, slow
-        while curr:
-            curr.next, prev, curr = prev, curr, curr.next
-    
-        
-        
-        # Merge Two Lists
-        first, second = head, prev
-        while second.next:
-            first.next, first = second, first.next
-            second.next, second = first, second.next
-        
+	def reorderList(self, head: ListNode) -> None:
+		if not head: return head
+		slow, fast = head, head
+		while fast.next and fast.next.next:
+			slow = slow.next
+			fast = fast.next.next
+		
+		prev, cur = None, slow.next
+		while cur:
+			save = cur.next
+			cur.next = prev
+			prev = cur
+			cur = save
+		slow.next = None
+		
+		head2 = prev
+		while head2:
+			save1 = head.next
+			head.next = head2
+			head = head2
+			head2 = save1
