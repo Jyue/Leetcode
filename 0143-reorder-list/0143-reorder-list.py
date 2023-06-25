cff58100e578d63c5271340fe50ce61e80b1284a
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-25 08:05:48 +0800

    Time: 108 ms (38.60%), Space: 26.1 MB (50.40%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,47 +1,52 @@
 class Solution:
-	def reorderList(self, head: Optional[ListNode]) -> None:
-	
-	# Don't be intimidated by the length, it's literally three easy questions combined you get to learn
-	# How to Find the Middle Node | Reverse A Linked List        | Merge Two Lists
-	# Find Middle Node: # 876     | Reverse Linked List: #206    | Merge Two Sorted Lists: #21
-	# It's one of the best linked list questions on here
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow = head
+        fast =head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+
+        prev, current = None, slow.next
+        # Using Example 2, node 3 will no longer point to 4, this splits the list in half
+        slow.next = None
+        # **** Reverse The Second Half ****
+        while current:
+            temp = current
+            current = current.next
+            temp.next = prev
+            prev = temp
+        first = head
+        second = prev
+        
+        while first and second:
+            if first:
+                buffer = first.next
+                first.next = second
+                first = buffer
+            if second:
+                buffer = second.next
+                second.next = first
+                second = buffer
 
 
-		# **** Find Middle Node ****
-		slow, fast = head, head
-		while fast and fast.next:
 
-			slow, fast = slow.next, fast.next.next       
 
-		prev, current = None, slow.next
-		# Using Example 2, node 3 will no longer point to 4, this splits the list in half
-		slow.next = None
-		# **** Reverse The Second Half ****
-		while current:
-			temp = current
-			current = current.next
-			temp.next = prev
-			prev = temp
+    # 		dummy = curr = ListNode(0)
+    # 		# head1 will be the start of the first list (node 1)
+    # 		# head2 will be the start of the second list (node 5)
+    # 		head1, head2 = head, prev
 
-		# **** Merge The Lists Together ****
+    # 		while head1 and head2:
+    # 			if head1:
+    # 				curr.next, head1 = head1, head1.next
+    # 				curr = curr.next
 
-		dummy = curr = ListNode(0)
-		# head1 will be the start of the first list (node 1)
-		# head2 will be the start of the second list (node 5)
-		head1, head2 = head, prev
+    # 			if head2:
+    # 				curr.next, head2 = head2, head2.next 
+    # 				curr = curr.next
 
-		while head1 and head2:
-			if head1:
-				curr.next, head1 = head1, head1.next
-				curr = curr.next
+        # whichever head1 or head2 still has a node remaining, we point to that remaining node
+        # curr.next = head1 or head2
 
-			if head2:
-				curr.next, head2 = head2, head2.next 
-				curr = curr.next
-
-		# whichever head1 or head2 still has a node remaining, we point to that remaining node
-		curr.next = head1 or head2
-
-		"""
-		Do not return anything, modify head in-place instead.
-		"""
+        """
+        Do not return anything, modify head in-place instead.
+        """
