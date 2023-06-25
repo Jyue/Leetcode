fec1dd96c48a8f6abe8cd7ca9fca790306f5abdb
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-25 10:31:35 +0800

    Time: 56 ms (11.42%), Space: 16.2 MB (89.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,5 +1,8 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        if len(s) % 2 == 1: 
+            return False
+        
         d = {'(':')', '{':'}','[':']'}
         stack = []
         for i in s:
@@ -8,4 +11,8 @@ class Solution:
             elif len(stack) == 0 or d[stack.pop()] != i:  # 2
                 return False
         return len(stack) == 0 # 3
+    
+# 1. 如果是 左括號 => 加到 stack
+# 2. 如果是 右括號 且 (1)stack 是空的(代表沒有matching的左括號) (2)或是 左括號不 match => return False
+# 3.  最後 check stack 中是否還有殘留的左括號
                     
