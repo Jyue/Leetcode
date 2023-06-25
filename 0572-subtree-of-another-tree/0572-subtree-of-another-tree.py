fc6b9d240880898ecdf8def894c2ef62b9be5893
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-25 09:59:03 +0800

    Time: 86 ms (95.67%), Space: 17.6 MB (53.53%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -5,22 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def isIdentical(r1,r2):
-            # If any one Node is Empty, both should be Empty
-            if r1 == None or r2 == None:
-                return r1 is None and r2 is None
-            return r1.val == r2.val and isIdentical(r1.left, r2.left) and isIdentical(r1.right, r2.right)
-        
-        # Preorder Traversal
-        def preorderTraversal(node):
-            if node is None:
-                return False
-            elif isIdentical(node, subRoot):
-                return True
-            
-            
-            return preorderTraversal(node.left) or preorderTraversal(node.right)
-        
-        return preorderTraversal(root)
-        
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        string_s = self.traverse_tree(s)
+        string_t = self.traverse_tree(t)
+        if string_t in string_s:
+            return True
+        return False
+    
+    
+    def traverse_tree(self, s):
+        if s:
+            return f"#{s.val} {self.traverse_tree(s.left)} {self.traverse_tree(s.right)}"
+        return None
