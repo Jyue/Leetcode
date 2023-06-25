c6ff691627c3ee0ef1389dd37114dd5216b32e1c
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-25 16:54:03 +0800

    Time: 107 ms (91.27%), Space: 17.7 MB (96.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,16 +2,18 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         n = numCourses
         graph = [[] for _ in range(n)]
-        g = [0] * n
+        # 計算每個 node 的 indegree。
+        indegree = [0] * n
         for v, u in prerequisites:
             graph[u].append(v)
-            g[v] += 1
-        S = [ v for v in range(n) if g[v] == 0]
+            indegree[v] += 1
+        # S 是可以參加的所有課程的stack。
+        S = [ v for v in range(n) if indegree[v] == 0]
         while S:
-            u = S.pop()
-            for v in graph[u]:
-                g[v] -= 1
-                if g[v] == 0:
-                    S.append(v)
-        return not any(g)
+            course = S.pop()
+            for nextCourse in graph[course]:
+                indegree[nextCourse] -= 1
+                if indegree[nextCourse] == 0:
+                    S.append(nextCourse)
+        return not any(indegree)
             
