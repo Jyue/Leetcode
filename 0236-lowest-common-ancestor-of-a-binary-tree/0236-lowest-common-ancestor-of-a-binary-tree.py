a111831224e249dd7e500bf84997d12fe6190153
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-25 16:20:50 +0800

    Time: 82 ms (73.69%), Space: 28.6 MB (56.78%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,47 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        def dfs(node):  # Return [How many target finded, the lowestCommonAncestor(TreeNode)]
-            # print(node)
-            if not node:
-                return (0,None)
-            
-            if not node.left and not node.right:
-                # print(node.val,"is a leaf",node.val == p or node.val == q)
-                return (1,None) if (node.val == p.val or node.val == q.val) else (0,None)
-            
-            
-            
-            left, leftAncestorFind = dfs(node.left)
-            right, rightAncestorFind = dfs(node.right)
-            
-            if leftAncestorFind or rightAncestorFind:
-                # print(node.val,"has found target")
-                if leftAncestorFind:
-                    return (2,leftAncestorFind)
-                else:
-                    return (2,rightAncestorFind)
-                
-            
-            if node.val == p.val or node.val == q.val:
-                count = 1 + left + right
-            else:
-                count = left + right
-            
-            # print(node.val, count)
-            return (2,node) if count == 2 else (count,None)
-   
-                
-        # print(dfs(root))
-        # return 0
-        _,lowestCommonAncestor = dfs(root)
-        # print(lowestCommonAncestor)
-        
-        return lowestCommonAncestor
+        # base case
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        # 情况 1
+        if left and right:
+            return root
+        # 情况 2
+        if not left and not right:
+            return None
+        # 情况 3
+        return right if not left else left
