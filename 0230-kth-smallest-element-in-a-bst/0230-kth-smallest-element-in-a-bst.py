7670f5317e6fab16c738191938d04d48e240205b
Cindy Lin <Jyue@users.noreply.github.com>
2023-06-25 11:48:07 +0800

    Time: 58 ms (91.30%), Space: 20.4 MB (73.37%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,21 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def kthSmallest(self, root: TreeNode, k: int) -> int:
-        # Traverse 到第 k 個就可以 return 了。故用Stack紀錄。
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
         stack = []
-        res = []
-        while stack or root:
+        
+        while True:
             while root:
                 stack.append(root)
                 root = root.left
             root = stack.pop()
-            res.append(root.val)
             k -= 1
             if not k:
-                return res[-1]
+                return root.val
             root = root.right
