56dbccca922e962d1acee59ac4ec43df9dcad6d3
Cindy Lin <Jyue@users.noreply.github.com>
2023-07-01 18:23:09 +0800

    Time: 136 ms (82.91%), Space: 17 MB (47.83%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -4,28 +4,26 @@ class Solution:
         count = 0
         m, n = len(grid), len(grid[0])
 
-        def dfs(i, j):
+        def detectEdge(i, j):
             if i < 0 or j < 0 or i >= m or j >= n:
                 # 超出索引邊界
-                return 1
+                return True
             if grid[i][j] == 1:
-                return 0
+                return False
             grid[i][j] = 1
             
-            return dfs(i + 1, j) | dfs(i, j + 1) | dfs(i - 1, j) | dfs(i, j - 1)
-            # result = 0
-            # result |= dfs(i + 1, j)
-            # result |= dfs(i, j + 1)
-            # result |= dfs(i - 1, j)
-            # result |= dfs(i, j - 1)
+        
+            # 注意不可用 or ，否則若有一者回傳True，後面不會被執行!!
+            return detectEdge(i + 1, j) | detectEdge(i, j + 1) | detectEdge(i - 1, j) | detectEdge(i, j - 1)
+         
             # return result
         
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0:
-                    notClosed = dfs(i, j)
-                    print(i,j,notClosed)
+                    notClosed = detectEdge(i, j)
+
                     count += 1 - notClosed
                     
         return count
